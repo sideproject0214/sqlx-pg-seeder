6e4988f72b0f7f6ee08394dbddb0050033eb5499
sideproject0214 <sideproject0214@gmail.com>
2023-12-27 15:33:11 +0900

    cargo homepage push

---
+++ docs/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"sqlx_pg_seeder":{"doc":"","t":"FF","n":["read_json_file","seeder"],"q":[[0,"sqlx_pg_seeder"],[2,"serde_json::value"],[3,"alloc::vec"],[4,"sqlx_postgres::database"],[5,"sqlx_core::pool"],[6,"core::error"],[7,"alloc::boxed"],[8,"core::result"]],"d":["",""],"i":[0,0],"f":[[[],[[2,[1]]]],[[[4,[3]]],[[8,[5,[7,[6]]]]]]],"c":[],"p":[[4,"Value",2],[3,"Vec",3],[3,"Postgres",4],[3,"Pool",5],[15,"tuple"],[8,"Error",6],[3,"Box",7],[4,"Result",8]]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
